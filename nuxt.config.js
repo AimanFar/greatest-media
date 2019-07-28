bc6c1ced3e29b9d69d211e8c6da6a3dde2670573
AimanFar <botheraiman@gmail.com>
2019-07-28 23:10:23 +0800

    Installed Fullhuman PurgeCss

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,23 @@
 
+const purgecss = require('@fullhuman/postcss-purgecss')
+const tailwind = require('tailwindcss')
+
+const postcssPlugins = [
+  tailwind(),
+]
+
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins,
+      },
+    },
+  },
+}
+
 export default {
   mode: 'universal',
   /*
@@ -9,7 +28,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'description', name: 'description', content: 'A full service branding and marketing agency that help E-commerce and retail companies achieve the growth that they want.' },
       { name: 'google-site-verification', content: 'gyNBpTIyKelNqYqAVFAkcmtprcDKBUvxH1bKdl9uhqU'},
     
     ],
@@ -43,7 +62,7 @@ export default {
   ],
   purgeCSS: {
     mode: 'postcss',
-    whitelist: ['responsive', 'group', 'inset-0','group-hover'],
+    whitelist: ['display', 'group-hover:flex'],
   },
   /*
   ** Build configuration
